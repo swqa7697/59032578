715ac9aee7596cf1e53565177f72dfd60a468b65
Shuzhan Xie <swqa7697@gmail.com>
2018-02-07 18:54:23 -0800

    Local Client
    Client is ready to play Locally.
    Waiting to be modified to online version.

---
+++ Client_Code/Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball: MonoBehaviour
+{
+    public float mySpeed;
+
+    private Rigidbody2D myRigidbody;
+    private Vector2 myDirection;
+
+	private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+        myDirection.Normalize();
+	}
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Top_bottom_edge")
+            myDirection.y = -myDirection.y;
+        if (other.gameObject.tag == "Left_right_edge")
+            myDirection.x = -myDirection.x;
+    }
+
+    private void move()
+    {
+        Vector2 movement = myDirection * mySpeed * Time.deltaTime;
+        myRigidbody.MovePosition(myRigidbody.position + movement);
+    }
+}
---
+++ Client_Code/Assets/Scripts/Player0.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player0: MonoBehaviour
+{
+    public float mySpeed, xMin, xMax;
+
+    private Rigidbody2D myRigidbody;
+    private Vector2 myDirection;
+
+	private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	private void FixedUpdate()
+    {
+        move();
+	}
+
+    private void move()
+    {
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            input += Vector2.left;
+        if (Input.GetKey(KeyCode.RightArrow))
+            input += Vector2.right;
+
+        input.Normalize();
+
+        Vector2 newPosition = myRigidbody.position + input * mySpeed * Time.deltaTime;
+        newPosition = new Vector2(Mathf.Clamp(newPosition.x, xMin, xMax), -4.8f);
+
+        myRigidbody.MovePosition(newPosition);
+    }
+}
